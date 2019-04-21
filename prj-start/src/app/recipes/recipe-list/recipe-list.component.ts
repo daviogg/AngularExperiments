cac3ef3402dd052da0be0cfda8914d8f6c75e319
daviogg <daviogg@hotmail.it>
2019-04-22 00:09:00 +0200

    Added buttons and shopping-list

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a simply test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*')
+    new Recipe('A test recipe', 'this is a simply test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*'),
+    new Recipe('A test recipe', 'this is a simply test', 'https://images-gmi-pmc.edge-generalmills.com/23bcd559-1e27-4c54-90a7-812690764c7f.jpg')
   ];
   constructor() { }
 
