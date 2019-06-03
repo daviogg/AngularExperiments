ef78f20bf8f45da30fc18e02f47cd0cb5937a02e
daviogg <daviogg@hotmail.it>
2019-06-03 21:09:14 +0200

    Services

---
+++ services-start/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AccountsService } from './accounts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  accounts : {name : string, status: string}[] = [];
+
+  /**
+   *
+   */
+  constructor(private accountsService: AccountsService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.accounts = this.accountsService.accounts;
+  }
+}
